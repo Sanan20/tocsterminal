b038ad90688dc6e470e5ea600c82557415df97d9
Sanan20 <113916891+Sanan20@users.noreply.github.com>
2023-12-27 11:07:00 +0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'welcome to the continental grounds mr wick!'
+    return 'welcome to the continental grounds john!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
